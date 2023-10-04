1ce40319a8c51600aeda7f9a13621d6d5dfd6603
James Hearn <53587635+Jimbobiss@users.noreply.github.com>
2023-10-04 21:51:21 +0100

    Add new js functionality for dvd bounce

--- myscript.js
+++ myscript.js
@@ -1,40 +1,56 @@
-function randomPosNum() {
-    const container = document.getElementById("container");
-    const xMax = container.offsetWidth;
-    const yMax = container.offsetHeight;
-    console.log(`${xMax} and ${yMax}`);
+let id = null;
 
+function randomPosNum(maxWidth, maxHeight) {
 
-    const xPos = Math.floor(Math.random() * xMax);
-    const yPos = Math.floor(Math.random() * yMax);
+    const xPos = Math.floor(Math.random() * maxWidth);
+    const yPos = Math.floor(Math.random() * maxHeight);
 
     const posArray = [xPos, yPos];
     console.log(posArray);
     return posArray;
 }
 
+//function executed when start button pressed
 function myMove() {
-    console.log("entered");
-    let id = null;
-    const elem = document.getElementById("animation");
-    let posArr = randomPosNum();
-    elem.style.left = posArr[0] + 'px';
-    elem.style.top = posArr[1] + 'px';
 
-    clearInterval(id);
-    id = setInterval(frame, 10);
-    function frame() {
-        if (posArr[0] <= 0 || posArr[0] >= 500) {
-            clearInterval(id);
-        } else {
-            posArr[0]++;
-            posArr[1]++;
+    if (!id) {
+        const container = document.getElementById("container");
+        const xMax = container.offsetWidth;
+        const yMax = container.offsetHeight;
 
-            elem.style.left = posArr[0] + 'px';
-            elem.style.top = posArr[1] + 'px';
-        }
+        const animatee = document.getElementById("animation");
+        const animSizeX = animatee.offsetWidth ;
+        const animSizeY = animatee.offsetHeight;
+
+        const elem = document.getElementById("animation");
+        let posArr = randomPosNum(xMax, yMax);
+        elem.style.left = posArr[0] + 'px';
+        elem.style.top = posArr[1] + 'px';
+
+        clearInterval(id);
+        id = setInterval(() => {
+
+            
+
+            /*
+            if (posArr[1] + animSizeY >= yMax  || posArr[0] + animSizeX >= xMax) {
+                clearInterval(id);
+            } else {
+                posArr[0]++;
+                posArr[1]++;
+
+                elem.style.left = posArr[0] + 'px';
+                elem.style.top = posArr[1] + 'px';
+            } */
+
+        }, 10);
     }
-
-    elem.style.left = posArr[0] + 'px';
-    elem.style.top = posArr[1] + 'px';
+    
 }
+
+function stopMove() {
+    clearInterval(id);
+    id = null;
+}
+
+document.getElementById("stop").addEventListener("onclick", stopMove);
